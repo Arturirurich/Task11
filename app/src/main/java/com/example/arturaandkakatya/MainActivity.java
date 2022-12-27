e140ceb568b82cf91ae4244140b9cfd06449b8e3
Arturirurich <arturkawin@gmail.com>
2022-12-27 15:02:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/arturaandkakatya/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.arturaandkakatya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements Printable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Cat cat = new Lion();
+        cat.talk();
+
+        Printable printable = new Puma();
+        printable.print();
+        ((Puma) printable).move();
+
+        printAnyObject(new MainActivity());
+    }
+
+
+    void printAnyObject(Printable printable){
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
---
+++ app/src/main/java/com/example/arturaandkakatya/Movable.java
@@ -0,0 +1,5 @@
+package com.example.arturaandkakatya;
+
+public interface Movable {
+    void move();
+}
